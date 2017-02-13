197eef823983bde7badd2615bbd5e385ecee638c
Theo Schlossnagle <jesus@lethargy.org>
2017-02-13 09:45:37 -0500

    put sidebars in the correct spot

--- themes/greyshade/source/js/sidebar.js
+++ themes/greyshade/source/js/sidebar.js
@@ -3,33 +3,9 @@
     $("a.sidebar-link").each(function() {
       var linkid = $(this).attr("id");
       var tgtid = '#' + linkid.replace(/^sidebar-link-/, "sidebar-");
-/*
-      (function(id, link) {
-        var timerId = null;
-        $(link).hover(function() {
-                        timerId = setTimeout(function() {
-                          $(".sidebar").not(id).addClass("sidebar-hidden");
-                          $(".sidebar-link").not(link).removeClass("sidebar-linklive");
-                          $(id).toggleClass("sidebar-hidden");
-                          $(link).addClass("sidebar-linklive");
-                        }, 500);
-                      },
-                      function() {
-                        if(timerId) {
-                          clearTimeout(timerId);
-                        }            
-                        timerId = null;
-                      });
-        $(link).click(function() {
-          //if(timerId) clearTimeout(timerId);
-          //timerId = null;
-          // $(".sidebar").not(id).addClass("sidebar-hidden");
-          // $(".sidebar-link").not(link).removeClass("sidebar-linklive");
-          //$(id).toggleClass("sidebar-hidden");
-          $(link).addClass("sidebar-linklive");
-        });
-*/
-      })(tgtid, this);
+      var aside = $(tgtid);
+      //aside.remove();
+      aside.insertAfter($(this));
     });
   });
 })(jQuery);
